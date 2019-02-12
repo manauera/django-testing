16dbf348348fc95545e12159f8dc8e4480678e9e
Gustavo Higuchi <gghiguchi@gmail.com>
2019-02-12 10:41:50 -0200

    JAVASCRIPT lvl 1: complete!

--- JAVASCRIPT_LEVEL_ONE/project.js
+++ JAVASCRIPT_LEVEL_ONE/project.js
@@ -6,6 +6,8 @@ var pet = prompt("Última pergunta. Qual o nome de seu bichinho de estimação?"
 
 if( (fname[0] == lname[0]) && (age > 20 && age < 30) && (height > 175) && (pet[pet.length - 1] === "y") ){
     console.log("Olá bonitão, vemk vem");
+} else{
+    console.log("Nada pra ver aqui... ");
 }
 
 alert("Muito bem! Obrigado e volte sempre!");
