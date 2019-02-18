a331666e8cb6d283f11e3c67b947bd18e5a68c47
Gustavo Higuchi <gghiguchi@gmail.com>
2019-02-18 16:11:46 -0300

    DJANGO lvl 1: first url (pycache removed)

--- DJANGO_LEVEL_ONE/first_project/first_project/urls.py
+++ DJANGO_LEVEL_ONE/first_project/first_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
+
 
 urlpatterns = [
+    path('', views.index, name="teste"),
     path('admin/', admin.site.urls),
 ]
