85670e3f72f6a3c934bf388ba576cf8e070c0bbc
Gustavo Higuchi <gghiguchi@gmail.com>
2019-02-19 19:42:39 -0300

    templates updated to get users

--- DJANGO_LVL_TWO/exercise/social/views.py
+++ DJANGO_LVL_TWO/exercise/social/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from social.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def users(request):
-    return render(request, 'users.html')
+    all_users = User.objects.order_by('fname')
+    dict = {'all_users': all_users}
+    return render(request, 'users.html', context=dict)
