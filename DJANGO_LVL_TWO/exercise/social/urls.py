ca1ede99265a4b881516179b40a9969458d1985d
Gustavo Higuchi <gghiguchi@gmail.com>
2019-02-19 19:20:02 -0300

    Paths set!

--- DJANGO_LVL_TWO/exercise/social/urls.py
+++ DJANGO_LVL_TWO/exercise/social/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from social import views
 
 urlpatterns = [
-    path('', include('social.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('users/', views.users),
 ]
