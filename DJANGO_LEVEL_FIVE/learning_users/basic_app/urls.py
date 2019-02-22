db0ee07863762093694b54c343accaa39cd3f548
Gustavo Higuchi <gghiguchi@gmail.com>
2019-02-22 16:16:46 -0300

    forms with relative templates and views configured of user register

--- DJANGO_LEVEL_FIVE/learning_users/basic_app/urls.py
+++ DJANGO_LEVEL_FIVE/learning_users/basic_app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from basic_app import views
 
+app_name = 'basic_app'
+
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('basic_app/', include('basic_app.urls')),
-    path('admin/', admin.site.urls),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name=""),
 ]
--- DJANGO_LEVEL_FIVE/learning_users/basic_app/views.py
+++ DJANGO_LEVEL_FIVE/learning_users/basic_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'basic_app/index.html')
+def login(request):
+    return render(request, 'basic_app/login.html')
+def register(request):
+    return render(request, 'basic_app/register.html')
